d08aaa36c0e4a7e36660830074122871c39f020b
GanangPrakoso <ganangmanggolo@gmail.com>
2020-01-14 01:48:11 +0700

    coba-coba function

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+function contohBos() {
+   let content = `
+   <div class="border" style="border-radius: 4px; overflow: auto; max-height: 80vh;">
+   <div class="mx-4 my-3">
+      <h4 style="font-family: 'Gelasio', serif">World Number two</h4>
+      <p class="mt-4" style="font-family: 'Gelasio', serif;"><i>Honestly
+            My eyes have never been so useless
+            Unconsciously
+            I dive into her warmth, her kindness
+            Sever me from the rope that's keeping up, tying me down
+            And here goes the free fall...
+            Suffocating
+            The thought of her suffocates me
+            Duplicates my worries and fears that she'll never be mine
+            Oh god help me, help me change her mind
+            Just let me sleep
+            So i can pick up where i left off
+            Rewind it back, we'll hope that it makes sense
+            And put the midnight fantasies all in repeat
+            Dreams of her takes me higher
+            Makes me decide to fall back asleep
+            Cause she's too far in world number one
+            She's my red rose in the other one
+            Dreams of her takes me higher
+            Beneath it all, she's all i could see
+            Cause she's too far in world number one
+            She's my red rose in the other one
+            Take it from me
+            I can't imagine you so beautiful
+            As clearly
+            Obviously
+            I petrify when you walk near me
+            You said that two wrongs make a right
+            But has our distance minimized?
+            One sec, i just blacked out
+            So here goes the free fall...
+            [Back to Chorus 1 &
+            Now breathe in the smoke
+            We'll find our way home
+            This hasn't happened yet
+            But it will, but it will.
+            We're silver and gold
+            We'll find our way home
+            Our hearts set to reset
+            Yeah it will, yeah it will
+            For the thrill
+            This heart keeps screaming louder
+            This whole town can hear it, the city knows it
+            She'll be my love in world number one
+            And keep her running the other one</i>
+      </p>
+   </div>
+</div>
+   `
+
+   $('#detail-article').html(content)
+}
